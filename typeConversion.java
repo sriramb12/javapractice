a6e552fa442264647dd4f8bbd0b23c458ce49a23
Sriram Bhamidipati <sriramb12@gmail.com>
2021-06-12 22:06:29 +0530

    first

---
+++ typeConversion.java
@@ -0,0 +1,16 @@
+public class typeConversion {
+    private static String x;
+   public static void main (String[] args) {
+         char c = 'B';
+         int i = c;
+         long l = i;
+         float f = l;
+         double d = l;
+         System.out.println("c = " + c);
+         System.out.println("i = " + i);
+         System.out.println("l = " + l);
+         System.out.println("f = " + f);
+         System.out.println("d = " + d);
+
+    }
+}
